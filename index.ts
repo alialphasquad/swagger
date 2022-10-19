e22fbd9904b2ea1b461113b437c946c876bd972d
alialphasquad <muhammadali.alphasquad@gmail.com>
2022-10-19 18:33:00 +0500

    first commit

--- index.ts
+++ index.ts
@@ -42,7 +42,10 @@ app.use("/parenttask", parentRouter);
 app.use("/workflow", workflowRouter);
 app.use("/stagetemplate", stageTemplate);
 
-
+app.get("/", (req: any, res: any) => {
+  res.send("Welcome to Home Page");
+  console.log("yo");
+});
 app.listen(3002, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${3002}`);
   console.log("yo");
